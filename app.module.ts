14504be1623f4ccf29c0de9326e950829ae50a11
freaknizzle999 <45108491+freaknizzle999@users.noreply.github.com>
2018-11-17 19:00:34 +0100

    Add files via upload
    hoop dat dit het is

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsideComponent } from './aside/aside.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    AsideComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
